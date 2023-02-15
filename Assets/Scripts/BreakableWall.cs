c0a1a4a10d8a565b73104183923c7074f5d9ec73
BasRuckebusch <97399311+BasRuckebusch@users.noreply.github.com>
2023-02-15 16:39:06 +0100

    Revert "Audio Player"

--- Assets/Scripts/BreakableWall.cs
+++ Assets/Scripts/BreakableWall.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class BreakableWall : MonoBehaviour
 {
 
-    [SerializeField][Min(0)] private float _impactAngle = 45.0f;
-    [SerializeField] private float _impactThreshold = 5.0f;
-    [SerializeField] private GameObject _wall;
-    [SerializeField] private BoxCollider2D _wallCollider2D;
-    [SerializeField] private ParticleSystem _particleSystem;
-    [SerializeField] private GameObject _brokenWall;
-    [SerializeField] private GameObject _parentGameObject;
-    [SerializeField] private AudioClip _shatterAudioClip;
-    // [SerializeField] private AudioSource _shatterAudioSource;
+	[SerializeField] [Min(0)] private float _impactAngle = 45.0f;
+	[SerializeField] private float _impactThreshold = 5.0f;
+	[SerializeField] private GameObject _wall;
+	[SerializeField] private BoxCollider2D _wallCollider2D;
+	[SerializeField] private ParticleSystem _particleSystem;
+	[SerializeField] private GameObject _brokenWall;
+	[SerializeField] private GameObject _parentGameObject;
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
@@ -45,38 +43,8 @@ public class BreakableWall : MonoBehaviour
 		return false;
 	}
 
-            if (CheckImpactAngle(playerVel, _impactAngle, _impactThreshold)) // Wall shattered!
-            {
-                Transform tr = _parentGameObject.GetComponentInParent<Transform>();
-                Debug.Log(tr.position);
-                var wallInst = Instantiate(_brokenWall, tr.position, tr.rotation);
-                wallInst.transform.localScale = tr.localScale;
-                wallInst.GetComponent<BrokenWall>().SetCollisionInvalid(other, playerVel.normalized);
-                _particleSystem.Play();
-                _wall.SetActive(false);
-                Invoke("DestroyObject", 1f);
-                //Play sound for shattering glass
-                if (_shatterAudioClip != null)
-                {
-                    FindFirstObjectByType<AudioPlayer>().PlayAudio(_shatterAudioClip);
-                }
-                //Destroy(_wall);
-            }
-        }
-    }
-
-    bool CheckImpactAngle(Vector3 impactVector, float impactAngle, float impactThreshold)
-    {
-        float playerSpeed = impactVector.magnitude;
-        if ((Vector3.Angle(impactVector, Vector3.left) > impactAngle) || (Vector3.Angle(impactVector, Vector3.right) > impactAngle) && playerSpeed > impactThreshold)
-        {
-            return true;
-        }
-        return false;
-    }
-
-    void DestroyObject()
-    {
-        Destroy(this);
-    }
+	void DestroyObject()
+	{
+		Destroy(this);
+	}
 }
