b5466a70701947e62555b539d435468b5fbda574
Vincent Van Denberghe <vincent.vd@icloud.com>
2023-02-15 16:10:08 +0100

    Disabled UI and Player layer collision

--- Assets/Scripts/BrokenWall.cs
+++ Assets/Scripts/BrokenWall.cs
@@ -7,23 +7,13 @@ public class BrokenWall : MonoBehaviour
 	private Collider2D _other;
 	[SerializeField] private float _force = 1000.0f;
 
-	//void OnCollisionEnter2D(Collision2D collision)
-	//{
-	//	if (!collision.gameObject.CompareTag("Player"))
-	//	{
-	//		return;
-	//	}
-	//	Vector2 collisionPos = collision.transform.position;
-	//	var parent = transform.parent.gameObject;
-	//	foreach (var collider2D in parent.GetComponentsInChildren<Collider2D>())
-	//	{
-	//		Vector2 collisionDir = collisionPos - new Vector2(collider2D.transform.position.x, collider2D.transform.position.y);
-	//		Vector2 forceDir = new Vector2((1 / -collisionDir.x) * _force , 0.0f);
-	//		collider2D.attachedRigidbody.AddForce(forceDir * 2.0f);
-	//		collider2D.attachedRigidbody.AddTorque(_force * 0.5f);
-	//	}
-	//	Invoke("Invalidate", 0.5f);
-	//}
+	void Start()
+	{
+		foreach (var collider2D in GetComponentsInChildren<Collider2D>())
+		{
+			Physics2D.IgnoreLayerCollision(5, 6);
+		}
+	}
 
 	public void SetCollisionInvalid(Collider2D other, Vector2 direction)
 	{
@@ -38,7 +28,7 @@ public class BrokenWall : MonoBehaviour
 			{
 				forceDir.x = 20.0f * Mathf.Sign(collisionDir.magnitude);
 			}
-			Debug.DrawLine(new Vector3(collisionPos.x, collisionPos.y, 0.0f), collider2D.transform.position, Color.red);
+			//Debug.DrawLine(new Vector3(collisionPos.x, collisionPos.y, 0.0f), collider2D.transform.position, Color.red);
 			collider2D.attachedRigidbody.AddForce(forceDir);
 			collider2D.attachedRigidbody.AddTorque(_force);
 			//Physics2D.IgnoreCollision(collider2D, _other);
