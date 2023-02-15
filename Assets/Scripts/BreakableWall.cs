db7721f45a06ac362f2ee7d74003c1ce4d01a908
narbys <aerts.sybran@gmail.com>
2023-02-15 16:48:43 +0100

    Revert "Audio Player"
    This reverts commit 71ecd6b5859690f1c09538b03bfcaa37cd33d87f.

--- Assets/Scripts/BreakableWall.cs
+++ Assets/Scripts/BreakableWall.cs
@@ -5,61 +5,48 @@ using UnityEngine;
 public class BreakableWall : MonoBehaviour
 {
 
-    [SerializeField][Min(0)] private float _impactAngle = 45.0f;
-    [SerializeField] private float _impactThreshold = 5.0f;
-    [SerializeField] private GameObject _wall;
-    [SerializeField] private BoxCollider2D _wallCollider2D;
-    [SerializeField] private ParticleSystem _particleSystem;
-    [SerializeField] private GameObject _brokenWall;
-    [SerializeField] private GameObject _parentGameObject;
-    [SerializeField] private AudioClip _shatterAudioClip;
-    // [SerializeField] private AudioSource _shatterAudioSource;
+	[SerializeField] [Min(0)] private float _impactAngle = 45.0f;
+	[SerializeField] private float _impactThreshold = 5.0f;
+	[SerializeField] private GameObject _wall;
+	[SerializeField] private BoxCollider2D _wallCollider2D;
+	[SerializeField] private ParticleSystem _particleSystem;
+	[SerializeField] private GameObject _brokenWall;
+	[SerializeField] private GameObject _parentGameObject;
 
-    private void Start()
-    {
-        // _shatterAudioSource = GetComponent<AudioSource>();
-        //_shatterAudioSource.clip = _shatterAudioClip;
-    }
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Vector3 playerVel = other.gameObject.GetComponent<Rigidbody2D>().velocity;
+			float playerSpeed = playerVel.magnitude;
+			
+			if (CheckImpactAngle(playerVel, _impactAngle, _impactThreshold)) // Wall shattered!
+			{
+				Transform tr = _parentGameObject.GetComponentInParent<Transform>();
+				Debug.Log(tr.position);
+				var wallInst = Instantiate(_brokenWall, tr.position, tr.rotation);
+				wallInst.transform.localScale = tr.localScale;
+				wallInst.GetComponent<BrokenWall>().SetCollisionInvalid(other, playerVel.normalized);
+				_particleSystem.Play();
+				_wall.SetActive(false);
+				Invoke("DestroyObject", 1.0f);
+				//Destroy(_wall);
+			}
+		}
+	}
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Vector3 playerVel = other.gameObject.GetComponent<Rigidbody2D>().velocity;
-            float playerSpeed = playerVel.magnitude;
+	bool CheckImpactAngle(Vector3 impactVector, float impactAngle, float impactThreshold)
+	{
+		float playerSpeed = impactVector.magnitude;
+		if ((Vector3.Angle(impactVector, Vector3.left) > impactAngle) || (Vector3.Angle(impactVector, Vector3.right) > impactAngle) && playerSpeed > impactThreshold)
+		{
+			return true;
+		}
+		return false;
+	}
 
-            if (CheckImpactAngle(playerVel, _impactAngle, _impactThreshold)) // Wall shattered!
-            {
-                Transform tr = _parentGameObject.GetComponentInParent<Transform>();
-                Debug.Log(tr.position);
-                var wallInst = Instantiate(_brokenWall, tr.position, tr.rotation);
-                wallInst.transform.localScale = tr.localScale;
-                wallInst.GetComponent<BrokenWall>().SetCollisionInvalid(other, playerVel.normalized);
-                _particleSystem.Play();
-                _wall.SetActive(false);
-                Invoke("DestroyObject", 1f);
-                //Play sound for shattering glass
-                if (_shatterAudioClip != null)
-                {
-                    FindFirstObjectByType<AudioPlayer>().PlayAudio(_shatterAudioClip);
-                }
-                //Destroy(_wall);
-            }
-        }
-    }
-
-    bool CheckImpactAngle(Vector3 impactVector, float impactAngle, float impactThreshold)
-    {
-        float playerSpeed = impactVector.magnitude;
-        if ((Vector3.Angle(impactVector, Vector3.left) > impactAngle) || (Vector3.Angle(impactVector, Vector3.right) > impactAngle) && playerSpeed > impactThreshold)
-        {
-            return true;
-        }
-        return false;
-    }
-
-    void DestroyObject()
-    {
-        Destroy(this);
-    }
+	void DestroyObject()
+	{
+		Destroy(this);
+	}
 }
