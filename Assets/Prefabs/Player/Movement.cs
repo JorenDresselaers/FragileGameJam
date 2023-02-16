40f1ade3254b79472c2d3308280b07e3e72a695c
Vincent Van Denberghe <vincent.vd@icloud.com>
2023-02-16 10:22:50 +0100

    Removed dying

--- Assets/Prefabs/Player/Movement.cs
+++ Assets/Prefabs/Player/Movement.cs
@@ -5,82 +5,82 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    const string HORIZONTAL = "Horizontal";
-    const string VERTICAL = "Vertical";
+	const string HORIZONTAL = "Horizontal";
+	const string VERTICAL = "Vertical";
 
-    const string LAYER_PLATFORM = "Platform";
+	const string LAYER_PLATFORM = "Platform";
 
-    private Rigidbody2D _rigidBody;
-    private BoxCollider2D _collider;
-    private SpriteRenderer _renderer;
+	private Rigidbody2D _rigidBody;
+	private BoxCollider2D _collider;
+	private SpriteRenderer _renderer;
 
-    private bool _isOnGround = false;
-    [SerializeField] private bool _canMove = false;
+	private bool _isOnGround = false;
+	[SerializeField] private bool _canMove = false;
 
-    [SerializeField] float _movementSpeed = 1;
+	[SerializeField] float _movementSpeed = 1;
 
-    public Animator _animator;
+	public Animator _animator;
 
-    void Awake()
-    {
-        _rigidBody = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<BoxCollider2D>();
-        _renderer = GetComponentInChildren<SpriteRenderer>();
-    }
+	void Awake()
+	{
+		_rigidBody = GetComponent<Rigidbody2D>();
+		_collider = GetComponent<BoxCollider2D>();
+		_renderer = GetComponentInChildren<SpriteRenderer>();
+	}
 
-    void Update()
-    {
-        if (_animator)
-            _animator.SetBool("IsGrounded", _isOnGround);
-        
-        if(_renderer) _renderer.flipX = _rigidBody.velocity.x < -1;
+	void Update()
+	{
+		if (_animator)
+			_animator.SetBool("IsGrounded", _isOnGround);
+		
+		if(_renderer) _renderer.flipX = _rigidBody.velocity.x < -1;
 
-        if (_canMove)
-        {
-            float horizontalMovement = Input.GetAxis(HORIZONTAL);
-            if (horizontalMovement != 0)
-            {
+		if (_canMove)
+		{
+			float horizontalMovement = Input.GetAxis(HORIZONTAL);
+			if (horizontalMovement != 0)
+			{
 
-                //_rigidBody.velocity = new Vector3(
-                //    Mathf.Clamp(_rigidBody.velocity.x + horizontalMovement * _movementSpeed, -10, 10),
-                //    _rigidBody.velocity.y);
+				//_rigidBody.velocity = new Vector3(
+				//    Mathf.Clamp(_rigidBody.velocity.x + horizontalMovement * _movementSpeed, -10, 10),
+				//    _rigidBody.velocity.y);
 
-                _rigidBody.AddForce(Vector2.right * horizontalMovement * _movementSpeed);
-            }
+				_rigidBody.AddForce(Vector2.right * horizontalMovement * _movementSpeed);
+			}
 
-            if (Input.GetAxis(VERTICAL) > 0)
-            {
-                if (_isOnGround) Jump(20);
-            }
-        }
-        else
-        {
-            float rotationDirection = Input.GetAxis(HORIZONTAL);
-            _rigidBody.rotation -= rotationDirection * _movementSpeed;
+			if (Input.GetAxis(VERTICAL) > 0)
+			{
+				if (_isOnGround) Jump(20);
+			}
+		}
+		else
+		{
+			float rotationDirection = Input.GetAxis(HORIZONTAL);
+			_rigidBody.rotation -= rotationDirection * _movementSpeed;
 
-        }
-    }
+		}
+	}
 
-    void Jump(float jumpForce)
-    {
-        if (!_rigidBody) return;
-        _rigidBody.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
-    }
+	void Jump(float jumpForce)
+	{
+		if (!_rigidBody) return;
+		_rigidBody.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+	}
 
-    private const string PlatformTag = "Platform";
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.collider.CompareTag(PlatformTag))
-        {
-            _isOnGround = true;
-        }
-    }
+	private const string PlatformTag = "Platform";
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.collider.CompareTag(PlatformTag))
+		{
+			_isOnGround = true;
+		}
+	}
 
-    void OnCollisionExit2D(Collision2D col)
-    {
-        if (col.collider.CompareTag(PlatformTag))
-        {
-            _isOnGround = false;
-        }
-    }
+	void OnCollisionExit2D(Collision2D col)
+	{
+		if (col.collider.CompareTag(PlatformTag))
+		{
+			_isOnGround = false;
+		}
+	}
 }
